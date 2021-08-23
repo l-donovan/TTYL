217f4e9436213f23ae0247f776307ac747c3481f
Luke Donovan <Luke.Donovan@viasat.com>
2021-08-23 09:27:48 -0700

    Initial Commit

---
+++ TTYL/TTYLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTYLApp.swift
+//  TTYL
+//
+//  Created by Luke Donovan on 8/23/21.
+//
+
+import SwiftUI
+
+@main
+struct TTYLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
